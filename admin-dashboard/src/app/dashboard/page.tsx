e5b7c04d34f6c85a611f6ff22aa0b3f5fa6ada66
Dineo Mathibela <dineomathibela161@gmail.com>
2024-05-08 23:27:44 +0200

    feat: init dashboard and navigatee from sign in button

---
+++ admin-dashboard/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { Dashboard } from "@/components";
+
+export default function Page() {
+    return (
+        <Dashboard />
+    )
+}
--- admin-dashboard/src/components/loginComponent.tsx
+++ admin-dashboard/src/components/loginComponent.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Image from "next/image";
+import Link from "next/link";
 
 export const Login = () => {
     return (
@@ -66,12 +67,12 @@ export const Login = () => {
                     </div>
 
                     <div>
-                        <button
-                            type="submit"
+                        <Link
+                            href={'/dashboard'}
                             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
                             Sign in
-                        </button>
+                        </Link>
                     </div>
                 </form>
             </div>
