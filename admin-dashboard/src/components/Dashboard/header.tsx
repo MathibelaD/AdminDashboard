9fae057eec86a8c1bf9f458e6269aade782990d5
Dineo Mathibela <dineomathibela161@gmail.com>
2024-05-09 02:11:24 +0200

    add shevron on dasborad top navigation

--- admin-dashboard/src/components/Dashboard/header.tsx
+++ admin-dashboard/src/components/Dashboard/header.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from "react";
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid';
 
 export const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -15,16 +16,14 @@ export const Header = () => {
                 {/* Insert your notification icon here */}
             </div>
             {/* User avatar and dropdown */}
-            <div className="flex items-center">
+            <div className="flex items-center rounded-full shadow-md p-2">
                 {/* User avatar */}
                 <img src="https://avatars.githubusercontent.com/u/92311415?v=4" alt="User Avatar" className="w-8 h-8 border rounded-full mr-2" />
                 {/* Dropdown menu */}
                 <div className="relative">
                     <button onClick={toggleDropdown} className="flex items-center focus:outline-none">
-                        <span className="mr-2">Ms mathibela</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M7.293 6.293a1 1 0 0 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 1 1 1.414-1.414L6 8.586V2a1 1 0 1 1 2 0v6.586l1.293-1.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z" clipRule="evenodd" />
-                        </svg>
+                        <span className="mr-2">Ms Mathibela</span>
+                        {!isOpen ? <ChevronDownIcon className="w-6 h-6" /> : <ChevronUpIcon className="w-6 h-6" />}
                     </button>
                     {isOpen && <div className="absolute right-0 mt-2 w-48 bg-white border rounded-md shadow-lg">
                         <button className="block px-4 py-2 text-sm text-gray-800 hover:bg-gray-200 w-full text-left">Admin Profile</button>
